45033c3fb3340a04e25a50d640e6b32ebfbd3253
Jeff Terrell <jeffaterrell@gmail.com>
2021-12-24 13:02:18 -0500

    add two classes to Models directory

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,12 @@
+namespace HairSalon.Models
+{
+  public class Client
+  {
+    public int ClientId { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Location {get; set; }
+    public int StylistId { get; set; }
+    public virtual Cuisine Stylist { get; set; }
+  }
+}
